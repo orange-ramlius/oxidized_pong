c95c5c5572394c460bba80bf5b1811a93dc68f85
bincell <vasram2706@gmail.com>
2024-06-26 23:40:00 +0400

    grid

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
 
+const WINDOW_WIDTH: f32 = 800.0;
+const WINDOW_HEIGHT: f32 = 600.0;
+
 pub struct App {
     gl: GlGraphics,
 }
@@ -18,22 +21,47 @@ impl App {
         use graphics::*;
 
         const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+
+        const REC_WIDTH: f32 = 10.0;
+        const REC_HEIGHT: f32 = 50.0;
 
         self.gl.draw(args.viewport(), |c, gl| {
             clear(BLACK, gl);
+
+            let transform = c.transform;
+
+            for i in 0..(600 / 50) {
+                if i % 2 == 0 {
+                    rectangle(
+                        WHITE,
+                        rectangle::rectangle_by_corners(
+                            (WINDOW_WIDTH / 2.0 - REC_WIDTH / 2.0).into(),
+                            (i as f32 * REC_HEIGHT).into(),
+                            (WINDOW_WIDTH / 2.0 + REC_WIDTH / 2.0).into(),
+                            (i as f32 * REC_HEIGHT + REC_HEIGHT).into(),
+                        ),
+                        transform,
+                        gl,
+                    );
+                }
+            }
         });
     }
 }
 
 fn main() {
     println!("Hello, world!");
-    let opengl = OpenGL::V3_1;
+    let opengl = OpenGL::V3_1; //3.1
 
-    let mut window: Window = WindowSettings::new("pong", [800, 600])
-        .graphics_api(opengl)
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
+    let mut window: Window = WindowSettings::new(
+        "pong",
+        [WINDOW_WIDTH.into(), WINDOW_HEIGHT.into()] as [f64; 2],
+    )
+    .graphics_api(opengl)
+    .exit_on_esc(true)
+    .build()
+    .unwrap();
 
     let mut app = App {
         gl: GlGraphics::new(opengl),
