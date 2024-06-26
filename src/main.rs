0f3d64ed9442ed2c41f1501e1948009e0e31d3c9
bincell <vasram2706@gmail.com>
2024-06-26 23:02:42 +0400

    open window, print hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::window::WindowSettings;
+
+pub struct App {
+    gl: GlGraphics,
+}
+
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            clear(BLACK, gl);
+        });
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    let opengl = OpenGL::V3_1;
+
+    let mut window: Window = WindowSettings::new("pong", [800, 600])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            app.render(&args);
+        }
+    }
 }
